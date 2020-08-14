38396c8120af7cf581e57db50ca0e5e3db0a659e
anushrishinde6 <anushrishinde6@gmail.com>
2020-08-14 21:34:45 +0530

    initial commit

---
+++ src/main/java/com/in28miunutes/microservices/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,21 @@
+package com.in28miunutes.microservices.limitsservice.bean;
+
+public class LimitConfiguration {
+	int maximum;
+	int minimum;
+	
+	protected LimitConfiguration(){
+		
+	}
+	public LimitConfiguration(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+	public int getMaximum() {
+		return maximum;
+	}
+	public int getMinimum() {
+		return minimum;
+	}
+}
